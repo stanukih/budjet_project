fbc71ec60aaf55556406619544b795d16111ef9c
Stanislav Ivkin <stanukih@gmail.com>
2025-12-04 10:16:42 +0200

    CategoryController

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    public function get_all(Request $request){
+        $categories = Category::all();
+        $response = [];
+            $response['status'] = 'success';
+            $response['data'] = $categories;
+            return response()->json($response, 400, ['Content-Type' => 'application/json;charset=UTF-8', 'Charset' => 'utf-8'],
+        JSON_UNESCAPED_UNICODE);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
@@ -8,6 +9,5 @@ use App\Http\Controllers\UserController;
 //Route::post('reg', 'UserController@registration');
 Route::post('/reg', [UserController::class, 'registration'])->name('registration');
 Route::post('/login', [UserController::class, 'login'])->name('login');
-
-
+Route::get('/category/get_all', [CategoryController::class, 'get_all'])->name('category_get_all'); 
 
