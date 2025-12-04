8e26570f80c79347695879346cb110306b79b868
Stanislav Ivkin <stanukih@gmail.com>
2025-12-04 13:46:25 +0200

    OperationController

---
+++ app/Http/Controllers/OperationController.php
@@ -0,0 +1,210 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Account;
+use App\Models\Operation;
+use Illuminate\Http\Request;
+
+class OperationController extends Controller
+{
+    public function create(Request $request){
+        if (
+            !isset($request['date'])&&
+            !isset($request['account_id'])&&
+            !isset($request['type'])&&
+            !isset($request['category_id'])&&
+            !isset($request['sum'])&&
+            !isset($request['description'])&&
+            !isset($request['user_id'])//&&
+            //!in_array($request['category_id'],['going','coming'])
+            
+            ){
+            $response = [];
+            $response['status'] = 'error';
+            $response['message'] = 'Riittämättömät tiedot';
+            return response()->json($response, 400, ['Content-Type' => 'application/json;charset=UTF-8', 'Charset' => 'utf-8'],
+        JSON_UNESCAPED_UNICODE);
+        }
+
+        $account = Account::find($request['account_id']);
+        if ($account->user_id!=$request['user_id']){
+
+        $response = [];
+            $response['status'] = 'error';
+            $response['message'] = 'Väärä pankkitili';
+            return response()->json($response, 400, ['Content-Type' => 'application/json;charset=UTF-8', 'Charset' => 'utf-8'],
+        JSON_UNESCAPED_UNICODE);
+        }
+
+        $operation = new Operation();
+        $operation->created_at = $request['date'];
+        $operation->account_id = $request['account_id'];
+        $operation->type = $request['type'];
+        $operation->category_id = $request['category_id'];
+        $operation->sum = $request['sum'];
+        $operation->description = $request['description'];
+        $operation->save();
+
+        $sum = $account->sum;
+        if ($request['type']=='going'){
+            $account->sum = $sum - $request['sum'];
+            $account->save();
+        }
+        if ($request['type']=='coming'){
+            $account->sum = $sum + $request['sum'];
+            $account->save();
+        }
+        $response = [];
+        $response['status'] = 'success';
+        return response()->json($response, 400, ['Content-Type' => 'application/json;charset=UTF-8', 'Charset' => 'utf-8'],
+        JSON_UNESCAPED_UNICODE);
+    }
+
+
+    public function get_all(Request $request){
+        if (!isset($request['user_id'])){
+            $response = [];
+            $response['status'] = 'error';
+            $response['message'] = 'Riittämättömät tiedot';
+            return response()->json($response, 400, ['Content-Type' => 'application/json;charset=UTF-8', 'Charset' => 'utf-8'],
+        JSON_UNESCAPED_UNICODE);
+        }
+        $accounts = Account::where('user_id', $request['user_id'])->get();
+        $operations_request = Operation::where('account_id','in',$accounts);
+        $filters = $request['filter'];
+        if (isset($filters['start_date'])){
+            $operations_request->where('created_at','>=',$filters['start_date']);
+        }
+        if (isset($filters['stop_date'])){
+            $operations_request->where('created_at','<=',$filters['stop_date']);
+        }
+        if (isset($filters['category_id'])){
+            $operations_request->where('category_id','=',$filters['category_id']);
+        }
+        if (
+            isset($filters['sum'])&&
+            isset($filters['sum']['operation'])&&
+            isset($filters['sum']['value'])
+            ){
+            $operations_request->where('sum',$filters['sum']['operation'],$filters['sum']['value']);
+        }
+        $operations = $operations_request->get();
+        $response = [];
+        $response['status'] = 'success';
+        $response['data'] = $operations;
+        return response()->json($response, 400, ['Content-Type' => 'application/json;charset=UTF-8', 'Charset' => 'utf-8'],
+        JSON_UNESCAPED_UNICODE);
+    }
+    public function update(Request $request){
+        if (
+            !isset($request['date'])&&
+            !isset($request['account_id'])&&
+            !isset($request['type'])&&
+            !isset($request['category_id'])&&
+            !isset($request['sum'])&&
+            !isset($request['description'])&&
+            !isset($request['operation_id'])&&
+            !isset($request['user_id'])
+            ){
+            $response = [];
+            $response['status'] = 'error';
+            $response['message'] = 'Riittämättömät tiedot';
+            return response()->json($response, 400, ['Content-Type' => 'application/json;charset=UTF-8', 'Charset' => 'utf-8'],
+        JSON_UNESCAPED_UNICODE);
+        }
+
+        $operation = Operation::find($request['operation_id']);
+        if ($operation->account_id!=$request['account_id']){
+            $response = [];
+            $response['status'] = 'error';
+            $response['message'] = 'Väärä pankkitili';
+            return response()->json($response, 400, ['Content-Type' => 'application/json;charset=UTF-8', 'Charset' => 'utf-8'],
+        JSON_UNESCAPED_UNICODE);
+        }
+
+        $account = Account::find($request['account_id']);
+        if ($account->user_id!=$request['user_id']){
+            $response = [];
+            $response['status'] = 'error';
+            $response['message'] = 'Väärä pankkitili';
+            return response()->json($response, 400, ['Content-Type' => 'application/json;charset=UTF-8', 'Charset' => 'utf-8'],
+        JSON_UNESCAPED_UNICODE);
+        }
+
+        //vanhan operaation peruuttaminen
+        if ($operation->type=='going'){
+            $account->sum = $account->sum + $operation->sum;
+            $account->save();
+        }
+        if ($operation->type=='coming'){
+            $account->sum = $account->sum - $operation->sum;
+            $account->save();
+        }
+        $operation->created_at = $request['date'];
+        $operation->account_id = $request['account_id'];
+        $operation->type = $request['type'];
+        $operation->category_id = $request['category_id'];
+        $operation->sum = $request['sum'];
+        $operation->description = $request['description'];
+        $operation->save();
+
+        if ($request['type']=='going'){
+            $account->sum = $account->sum - $request['sum'];
+            $account->save();
+        }
+        if ($request['type']=='coming'){
+            $account->sum = $account->sum + $request['sum'];
+            $account->save();
+        } 
+        $response = [];
+        $response['status'] = 'success';
+        return response()->json($response, 400, ['Content-Type' => 'application/json;charset=UTF-8', 'Charset' => 'utf-8'],
+        JSON_UNESCAPED_UNICODE);
+    }
+
+    public function delete(Request $request){
+        if (
+            !isset($request['operation_id'])&&
+            !isset($request['user_id'])
+            ){
+            $response = [];
+            $response['status'] = 'error';
+            $response['message'] = 'Riittämättömät tiedot';
+            return response()->json($response, 400, ['Content-Type' => 'application/json;charset=UTF-8', 'Charset' => 'utf-8'],
+        JSON_UNESCAPED_UNICODE);
+        }
+
+        $operation = Operation::find($request['operation_id']);
+        if (!$operation){
+            $response = [];
+            $response['status'] = 'error';
+            $response['message'] = 'Väärä operation_id';
+            return response()->json($response, 400, ['Content-Type' => 'application/json;charset=UTF-8', 'Charset' => 'utf-8'],
+        JSON_UNESCAPED_UNICODE);
+        }
+        $account = Account::find($operation->account_id);
+        if ($account->user_id!=$request['user_id']){
+            $response = [];
+            $response['status'] = 'error';
+            $response['message'] = 'Väärä user_id';
+            return response()->json($response, 400, ['Content-Type' => 'application/json;charset=UTF-8', 'Charset' => 'utf-8'],
+        JSON_UNESCAPED_UNICODE);
+        }
+
+        //vanhan operaation peruuttaminen
+        if ($operation->type=='going'){
+            $account->sum = $account->sum + $operation->sum;
+            $account->save();
+        }
+        if ($operation->type=='coming'){
+            $account->sum = $account->sum - $operation->sum;
+            $account->save();
+        }
+        $operation->delete();
+        $response = [];
+        $response['status'] = 'success';
+        return response()->json($response, 400, ['Content-Type' => 'application/json;charset=UTF-8', 'Charset' => 'utf-8'],
+        JSON_UNESCAPED_UNICODE);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\AccountController;
 use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\OperationController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
@@ -20,3 +21,8 @@ Route::get('/account/get_all', [AccountController::class, 'get_all'])->name('acc
 Route::post('/account/create', [AccountController::class, 'create'])->name('account_create');
 Route::put('/account/update', [AccountController::class, 'update'])->name('account_update');
 
+//Operation
+Route::get('/operation/get_all', [OperationController::class, 'get_all'])->name('operation_get_all'); 
+Route::post('/operation/create', [OperationController::class, 'create'])->name('operation_create');
+Route::put('/operation/update', [OperationController::class, 'update'])->name('operation_update');
+Route::delete('/operation/delete', [OperationController::class, 'delete'])->name('operation_delete');
